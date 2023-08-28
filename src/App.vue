4eab5cf76e771872d9c832b7bddc587f63fc4693
ranggardb <ranggardb@gmail.com>
2023-08-28 08:38:26 +0700

    delete unused

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,11 @@
 <template>
   <div>
     <RouterView />
-    <!-- <router-view></router-view> -->
-    <!-- <Login /> -->
-    <!-- <ForgotPassword /> -->
-    <!-- <User /> -->
   </div>
 </template>
 
 <script>
-import ForgotPassword from './pages/Auth/ForgotPassword.vue';
-import Login from './pages/Auth/Login.vue';
-import User from './pages/Auth/User.vue';
-
-export default {
-  components: { Login, ForgotPassword, User },
-};
+export default {};
 </script>
 
 <style scoped></style>
